d277850c973f44840643f4d555a5671f11de58ca
Kai120789 <zoomer120789@gmail.com>
2026-01-23 03:03:57 +0300

    feat: run http server, added gateway to general docker compose

--- learning_platform_api_gateway/internal/app/app.go
+++ learning_platform_api_gateway/internal/app/app.go
@@ -1,5 +1,54 @@
 package app
 
-func Start() {
+import (
+	"fmt"
+	"learning-platform/api-gateway/internal/config"
+	"learning-platform/api-gateway/internal/redis"
+	"learning-platform/api-gateway/pkg/logger"
+	"net/http"
+	"sync"
 
+	"go.uber.org/zap"
+)
+
+func Start() {
+	cfg := config.GetConfig()
+
+	zapLog, err := logger.New(cfg.LogLevel)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	log := zapLog.ZapLogger
+
+	redisConn, err := redis.Connection(cfg.RedisUrl)
+	if err != nil {
+		log.Fatal("error connect to redis", zap.Error(err))
+	}
+
+	_ = redisConn
+
+	// TODO: init handler
+
+	// TODO: init router
+
+	server := &http.Server{
+		Addr:    cfg.ServerAddress,
+		Handler: http.NewServeMux(),
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		log.Info("http server started", zap.String("address", cfg.ServerAddress))
+		if err := server.ListenAndServe(); err != nil {
+			log.Error("failed to start http server", zap.Error(err))
+		}
+	}()
+
+	// TODO: start grpc
+
+	wg.Wait()
 }
