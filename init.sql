d92061a6315ed69d375a47b979e9f29202f454bc
Kai120789 <zoomer120789@gmail.com>
2026-06-25 17:43:36 +0300

    feat: added migration command for group service and db creation to init.sql

--- init.sql
+++ init.sql
@@ -4,3 +4,11 @@ GRANT ALL PRIVILEGES ON DATABASE user_db TO user_db_user;
 
 \c user_db
 GRANT ALL ON SCHEMA public TO user_db_user;
+
+
+CREATE DATABASE group_db;
+CREATE USER group_db_user WITH PASSWORD 'group_db_password';
+GRANT ALL PRIVILEGES ON DATABASE group_db TO group_db_user;
+
+\c group_db
+GRANT ALL ON SCHEMA public TO group_db_user;
