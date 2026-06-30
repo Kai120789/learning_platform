2ccfc7d6dfc303893d0c7a57348d3e249d10facd
Kai120789 <zoomer120789@gmail.com>
2026-06-30 15:42:54 +0300

    feat: added schedule service init migrations, commands for it and migrate all, schedule service starting in docker compose

--- init.sql
+++ init.sql
@@ -28,3 +28,11 @@ GRANT ALL PRIVILEGES ON DATABASE subject_db TO subject_db_user;
 
 \c subject_db
 GRANT ALL ON SCHEMA public TO subject_db_user;
+
+
+CREATE DATABASE schedule_db;
+CREATE USER schedule_db_user WITH PASSWORD 'schedule_db_password';
+GRANT ALL PRIVILEGES ON DATABASE schedule_db TO schedule_db_user;
+
+\c schedule_db
+GRANT ALL ON SCHEMA public TO schedule_db_user;
