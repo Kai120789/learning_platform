a3228a854a2aadaa7c8cd0967eebd8053347729d
Kai120789 <zoomer120789@gmail.com>
2026-07-30 12:29:15 +0300

    feat: update api gateway for new schedule service logic

---
+++ learning_platform_api_gateway/internal/dto/scheduleDto/createScheduleSlot.go
@@ -0,0 +1,11 @@
+package scheduleDto
+
+import (
+	"time"
+)
+
+type CreateScheduleSlot struct {
+	ScheduleID int64     `json:"schedule_id"`
+	StartTime  time.Time `json:"start_time"`
+	Duration   *int64    `json:"duration"`
+}
